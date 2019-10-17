26f8cc33f2c53af574031f3a8a441ac38a2a8dcb
sampie926 <56376288+sampie926@users.noreply.github.com>
2019-10-17 10:21:20 +0700

    a

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
  <body>
  <?php echo '<p style="color: red">Hello World</p>'; ?> 
  <img src="src/ricardo.jfif">
+ <a href="second.php" class="ui-btn">Second page</a>
  </body>
 </html>
---
+++ second.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p style="color: red">THis is second page</p>'; ?> 
+ <img src="src/ricardo.jfif">
+ </body>
+</html>
